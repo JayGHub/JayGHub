0a30ad2683f77a5b9bb8f77f8b9df9dc9ea9bdda
Joshua Green <lovetoexplore@outlook.com>
2025-06-17 12:08:07 -0400

    Update AboutMe.java

--- AboutMe_Java/AboutMe.java
+++ AboutMe_Java/AboutMe.java
@@ -8,20 +8,22 @@ public class AboutMe {
         boolean done = false;
         String message = new String();
         while (!done) {
-            System.out.print("What would you like to know about me? ");
+            System.out.print("What would you like to know about me? All spaces must be replaced by underscore");
             String question = input.next();
             while (!found) {
                 found = true;
                 switch (question) {
+                    case "Name": message = "My name is Joshua Green"; break;
                     case "Discord": message = "I have 3 Discord accounts\nMy main account (I have full friends): joshua_green\nMy second account: dynoalt\nMy third account: dynoalt2"; break;
                     case "Discord_Server": message = "My Discord server link is https://www.discord.gg/yA9sQt7AuW \nOr you can use the code: yA9sQt7AuW"; break;
                     case "Snapchat": message = "My Snapchat is jaysnappychatt\nYou could also use the link: https://www.snapchat.com/add/jaysnappychatt?share_id=aC_7Xxwjf4o&locale=en-US"; break;
-                    case "Age": message = "I am 17 years old"; break;
+                    case "Age": message = "I am 18 years old"; break;
                     case "Number": message = "My phone number is (321)393-5321"; break;
                     case "Birthday": message = "My birthday is April 23, 2007"; break;
                     case "Sexuality": message = "I am bisexual"; break;
                     case "Status": message = "I am single"; break;
                     case "YouTube": message = "My youtube channel links are 7 ,u89"; break;
+                    case "help": message = "Name, Age, Birthday, Number, Sexuality, Status, Discord, Discord_Server, Snapchat, YouTube"; break;
                     default: System.out.print("Couldn't find what you are looking for! Please try again. "); question = input.nextLine(); found = false; break;
                 }
             }
